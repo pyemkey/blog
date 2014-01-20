d228357df71d3a23545c3850f0d2a7c96fa019dc
pyemkey <py.emkey@gmail.com>
2014-01-20 11:56:07 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,32 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.2'
+
+gem 'sqlite3'
+
+gem 'bootstrap-saas'
+gem 'sass-rails', '~> 4.0.0'
+
+gem 'devise'
+gem 'figaro'
+gem 'better_errors'
+gem 'quiet_assets'
+
+gem 'uglifier', '>= 1.3.0'
+
+gem 'coffee-rails', '~> 4.0.0'
+
+gem 'simple_form'
+
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloger::Application.config.secret_key_base = 'adb87735603533475d6e20d0d73b607b2bbab6db9328dbf8cba37340dfca003a22b8bb75aa46bebbe02e72c1919dd2783267551f68c7af3cf7d198249e54c2cf'
